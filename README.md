49078df93974f5a298c54c53f5666ad4af4962b1
Ben Lykins <benjamin.m.lykins@gmail.com>
2024-04-18 12:27:19 -0400

    Add Terraform plan step to Azure Windows 2019 base workflow

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # packer-demos
-touc
---
+++ terraform/main.tf
@@ -0,0 +1,67 @@
+terraform {
+  cloud {
+    organization = "RPTData"
+
+    workspaces {
+      name = "cli-workspace"
+    }
+  }
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.99.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "windows" {
+  source  = "Azure/virtual-machine/azurerm"
+  version = "1.1.0"
+
+  name                = "ben-win-vm"
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.this.name
+  size                = "Standard_DS1_v2"
+  image_os            = "windows"
+  os_simple           = "WindowsServer"
+  subnet_id           = azurerm_subnet.this.id
+
+  admin_password = sensitive(random_password.this.result)
+
+  os_disk = {
+    name                 = "ben-wim-vm-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+}
+
+
+resource "random_password" "this" {
+  length = 24
+}
+
+resource "azurerm_virtual_network" "this" {
+  address_space       = ["10.0.0.0/16"]
+  location            = "eastus"
+  name                = "ben-vnet"
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+resource "azurerm_subnet" "this" {
+  address_prefixes     = ["10.0.0.0/24"]
+  name                 = "ben-subnet"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = "ben-rg"
+  location = "eastus"
+}
