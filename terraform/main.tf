cfb0c980948894ef491473e71f7897373a510e54
Ben Lykins <benjamin.m.lykins@gmail.com>
2024-04-22 20:28:20 -0400

    add validates

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,7 +30,7 @@ module "windows" {
   resource_group_name = azurerm_resource_group.this.name
   size                = "Standard_DS1_v2"
   image_os            = "windows"
-  os_simple           = "WindowsServer"
+  source_image_id     = "value"
   subnet_id           = azurerm_subnet.this.id
 
   admin_password = sensitive(random_password.this.result)
