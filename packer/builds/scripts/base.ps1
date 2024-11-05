42de8bbb7274cd19a9f849a300325dad3d1f0eb8
Benjamin lykins <91494226+benjamin-lykins@users.noreply.github.com>
2024-11-05 12:57:27 -0500

    remove obsolete files and add new Packer variable configurations (#1)

--- packer/builds/scripts/base.ps1
+++
@@ -1,5 +0,0 @@
-Write-Output '=== Installing Mondoo ==='
-Create-Item -type directory -path 'C:\packer'
-Set-Location -Path 'C:\packer'
-Invoke-WebRequest -Uri https://releases.mondoo.com/mondoo/10.12.2/mondoo_10.12.2_windows_amd64.msi -OutFile mondoo.msi
-Start-Process -Wait msiexec -ArgumentList "/qn /i mondoo.msi REGISTRATIONTOKEN=$env:MONDOO_REGISTRATION_TOKEN"
